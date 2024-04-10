54d46c1e10c1ac3a8116a8d89c65a901fe2840f0
dasha <jjg-lessons@gmail.com>
2024-04-10 22:22:35 +0300

    Initial commit

---
+++ common/src/com/itmo/common/Message.java
@@ -0,0 +1,37 @@
+package com.itmo.common;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private String sender;
+    private String text;
+    private LocalDateTime sentAt;
+
+    public Message(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+}
